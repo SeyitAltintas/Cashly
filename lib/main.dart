9aee128956d393e1af050f7804fda12eb30104f0
Seyit <seyitaltintas8@gmail.com>
2026-02-28 18:08:50 +0300

    run-e2e

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,6 @@ class CashlyApp extends StatefulWidget {
 }
 
 class _CashlyAppState extends State<CashlyApp> with WidgetsBindingObserver {
-  AuthController? _authController;
   AppRouter? _appRouter;
   bool _isInitialized = false;
   String? _initError;
@@ -160,7 +159,6 @@ class _CashlyAppState extends State<CashlyApp> with WidgetsBindingObserver {
 
       if (mounted) {
         setState(() {
-          _authController = authController;
           _appRouter = AppRouter(authController: authController);
           _isInitialized = true;
         });
