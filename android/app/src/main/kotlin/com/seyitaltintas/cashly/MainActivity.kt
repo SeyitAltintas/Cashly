e9300847b6674b890122e3f7dcfe4a6a9183256a
Seyit <seyitaltintas8@gmail.com>
2026-06-09 21:00:47 +0300

    security: prevent screenshots and screen recording (FLAG_SECURE)

--- android/app/src/main/kotlin/com/seyitaltintas/cashly/MainActivity.kt
+++ android/app/src/main/kotlin/com/seyitaltintas/cashly/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.seyitaltintas.cashly
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Red Team Yaması: Ekran görüntüsü almayı, ekran kaydını ve 
+        // son uygulamalar (recents) menüsünde uygulamanın önizlemesinin görünmesini engeller.
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
